9a886fa9d50f94ff3a5d91b688b868ffbdbdd758
CoddingNes <axela.n@wanadoo.fr>
2022-09-29 12:10:27 +0200

    Modif méthode de création d'éléments (retrait du innerHTML) dans script.js

--- front/js/script.js
+++ front/js/script.js
@@ -1,11 +1,33 @@
+//Appel de l'API
 fetch(`http://localhost:3000/api/products/`)
   .then((response) => response.json())
   .then((data) => {
     
     console.table(data)
+
+//Création des articles de chaque item    
     for (let i = 0; i < data.length; i++) {
-        let items = document.querySelector("#items");
-        let htmlCard = `
+        const sectionItemElement = document.querySelector("#items");
+        const lienItemElement = document.createElement("a");
+        lienItemElement.setAttribute("href", data[i]._id);
+        const articleItemElement = document.createElement("article");
+        const imgItemElement = document.createElement("img");
+        imgItemElement.setAttribute("src", data[i].imageUrl);
+        imgItemElement.setAttribute("alt", data[i].altTxt);
+        const h3ItemElement = document.createElement("h3");
+        h3ItemElement.setAttribute("className", "productName");
+        h3ItemElement.innerText = data[i].name;
+        const pItemElement = document.createElement("p");
+        pItemElement.setAttribute("className", "productDescription");
+        pItemElement.innerText = data[i].description;
+
+        sectionItemElement.appendChild(lienItemElement);
+        lienItemElement.appendChild(articleItemElement);
+        articleItemElement.append(imgItemElement, h3ItemElement, pItemElement);
+
+
+/*
+        const htmlCard = `
           <a href="./product.html?id=${data[i]._id}"> 
             <article> 
               <img src="${data[i].imageUrl}" alt="${data[i].altTxt}"> 
@@ -14,5 +36,6 @@ fetch(`http://localhost:3000/api/products/`)
             </article> 
           </a>`;
         items.innerHTML += htmlCard
+*/
     }
-    });
+});
