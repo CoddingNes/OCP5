06f1052299b05dca3abdda425a08f981b229ae4b
CoddingNes <axela.n@wanadoo.fr>
2022-09-30 23:43:21 +0200

    Retrait fct sort() et ajout détection qttés incorrectes dans product.js

--- front/js/product.js
+++ front/js/product.js
@@ -18,76 +18,39 @@ fetch(`http://localhost:3000/api/products/${id}`)
   .then((response) => response.json())
   .then((data) => {
     
-//    console.log(data);
-
-//Comparaison des ID de l'API à l'ID en localstorage
-//    for (let i = 0; i < data.length; i++) {
-//        if (data_id == id) {
-
 //Intégration des données de l'API selon l'ID identifié
-            imgElement.setAttribute('src', data.imageUrl);
-            imgElement.setAttribute('alt', data.altTxt);
-            titleElement.innerHTML = data.name;
-            priceElement.innerHTML = data.price;
-            descriptionElement.innerHTML = data.description;
+    imgElement.setAttribute('src', data.imageUrl);
+    imgElement.setAttribute('alt', data.altTxt);
+    titleElement.innerHTML = data.name;
+    priceElement.innerHTML = data.price;
+    descriptionElement.innerHTML = data.description;
 
 //Ajout du nombre d'options correspondant au nombre de couleurs disponibles pour ce produit           
-            const productColors = data.colors;
-            for (let i = 0; i < productColors.length; i++) {
-                const colorOptions = document.createElement('option');
-                colorsElement.appendChild(colorOptions);
-                colorOptions.setAttribute('value', productColors[i]);
-                colorOptions.innerHTML = productColors[i];
-            }
-//        }
-//    }
+    const productColors = data.colors;
+    for (let i = 0; i < productColors.length; i++) {
+        const colorOptions = document.createElement('option');
+        colorsElement.appendChild(colorOptions);
+        colorOptions.setAttribute('value', productColors[i]);
+        colorOptions.innerHTML = productColors[i];
+    }
 });
 
 const boutton = document.querySelector('#addToCart');
 const colors = document.querySelector('#colors');
 const quantity = document.querySelector('#quantity');
 
-//Fonction appliquée si le panier n'est pas vide
-const addOrPush = (cart) => {
-    let addNew = true; //On/Off pour ajouter nouvel item, passe en off s'il existe
-    cart.forEach(item => {
-        if (item.id == id && item.colors === colors.value) {
-        console.log("L'item existe déjà dans le panier, incrémentons-le");
-        item.quantity = parseInt(item.quantity) + parseInt(quantity.value);
-        localStorage.setItem("cart", JSON.stringify(cart));
-        addNew = false;
-        }
-    });
-    if (addNew) {
-    console.log("Ajoutons l'item au panier");
-    cart.push({
-        id : id,
-        quantity : quantity.value,
-        colors : colors.value
-    });
-    localStorage.setItem("cart", JSON.stringify(cart));
-    };
-};
-
-//Tri des items du Cart par ID
-const sortCart = () => {
-    if (cart.length > 1) {
-        cart.sort((x, y) => {
-            if (x.id < y.id) {return -1;}
-            if (x.id > y.id) {return 1;}
-            return 0;
-        });
-        localStorage.setItem("cart", JSON.stringify(cart));
-    }
-}
-    
 //Ajout des produits au panier
-boutton.addEventListener('click', (e) => {
-    if (quantity.value < 1 || quantity.value > 100 || colors.value == "") {
-        alert("Veuillez saisir un nombre d'article entre 1 et 100 et choisir une couleur.");
+boutton.addEventListener('click', (e) => { 
+    if (colors.value == "") {
+        alert("Veuillez choisir une couleur.");
+    } else if (quantity.value == 0) {
+        alert("Attention! Vous n'avez pas saisi de quantité!");
     } else {
 //Si le panier est vide, créer tableau en ajoutant les données
         if (!localStorage.getItem('cart')) {
+            if (quantity.value < 1 || quantity.value > 100 || colors.value == "") {
+                alert("Veuillez saisir un nombre d'article entre 1 et 100 et choisir une couleur.");
+            } else {
             console.log("Créons le panier!");
             let cart = [{
                 id : id,
@@ -95,28 +58,49 @@ boutton.addEventListener('click', (e) => {
                 colors : colors.value
             }];
             localStorage.setItem("cart", JSON.stringify(cart))
+            alert("Votre produit a bien été ajouté au panier");
+            }
         }
 //Si le panier n'est pas vide 
-        else{
+        else {
             console.log("Le panier existe déjà")
             cart = JSON.parse(localStorage.getItem('cart'));
             addOrPush(cart)
         }
-        alert("Votre produit a bien été ajouté au panier");
-        sortCart();
     }
 });
 
-/*
-//Tri des items du Cart par ID
-if (cart.length > 1) {
-    function sortCart(x, y){
-        if (x.id < y.id) {return -1;}
-        if (x.id > y.id) {return 1;}
-        return 0;
-    }
-    cart.sort(sortCart);
-    localStorage.setItem("cart", JSON.stringify(cart));
-}
-*/
 
+//Fonction appliquée si le panier n'est pas vide
+const addOrPush = (cart) => {
+    let addNew = true; //True pour ajouter nouvel item, passe en False s'il existe déjà
+    cart.forEach(item => {
+        if (item.id == id && item.colors === colors.value) {
+            addNew = false;
+            if ((item.quantity + quantity.value) > 100) {
+                let retirerItem = (item.quantity + quantity.value) - 100;
+                alert(`Le nombre total de cet article doit être compris entre 1 et 100, veuillez retirer au moins ${retirerItem} article(s)`);
+            } else if ((item.quantity + quantity.value) < 0) {
+                let ajouterItem = (item.quantity + quantity.value);
+                alert(`Le nombre total de cet article doit être compris entre 1 et 100, veuillez ajouter au moins ${ajouterItem} article(s)`);
+            } else {
+                console.log("L'item existe déjà dans le panier, incrémentons-le");
+                item.quantity = parseInt(item.quantity) + parseInt(quantity.value);
+
+                localStorage.setItem("cart", JSON.stringify(cart));
+            }
+        } else if (quantity.value < 1 || quantity.value > 100) {
+            alert("Veuillez saisir un nombre d'article entre 1 et 100 et choisir une couleur.");
+        } 
+    });
+    if (addNew) {
+        console.log("Ajoutons l'item au panier");
+        cart.push({
+            id : id,
+            quantity : quantity.value,
+            colors : colors.value
+        });
+        localStorage.setItem("cart", JSON.stringify(cart));
+    };
+    alert("Votre produit a bien été ajouté au panier");
+};
